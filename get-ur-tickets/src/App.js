56ded4517965425755e037abbfdca976864759e2
Alexander Blake <alex.blake24@gmail.com>
2025-01-02 00:01:59 -0500

    Removed Login Requirement

--- get-ur-tickets/src/App.js
+++ get-ur-tickets/src/App.js
@@ -1,40 +1,25 @@
 import React from 'react';
 import './App.css';
 import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { Contact } from './pages/Contact Us';
 import { Routes, Route } from 'react-router-dom';
-import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './Components/Layout';
 
 const App = () => {
-  const { user } = useAuthenticator(); // Get user info via useAuthenticator hook
 
   return (
-    // Wrap the app in the Authenticator.Provider
-    <Authenticator.Provider>
       <div style={{ backgroundColor: '#0F0F0F', minHeight: '100vh', color: 'white', paddingTop: 75}}>
         <Layout>
           <Routes>
-            {/* Check if user is logged in */}
-            {user ? (
+            {(
               <>
                 <Route path="/Home" element={<Home />} />
-                <Route path="/Contact Us" element={<Contact />} />
-                <Route path="/" element={<Home />} /> {/* Default route when user is logged in */}
-              </>
-            ) : (
-              <>
-                <Route path="/" element={<Login />} />
-                <Route path="/Home" element={<Login />} />
-                <Route path="/Contact Us" element={<Login />} />
+                <Route path="/" element={<Home />} />
               </>
             )}
           </Routes>
         </Layout>
       </div>
-    </Authenticator.Provider>
   );
 };
 
--- get-ur-tickets/src/Components/Footer.js
+++ get-ur-tickets/src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-dark text-white py-3 mt-5">
             <div className="container text-center">
-                <p>&copy; 2024 Get Ur Tickets. All Rights Reserved.</p>
+                <p>&copy; 2025 Get Ur Tickets. All Rights Reserved.</p>
             </div>
         </footer>
     );
